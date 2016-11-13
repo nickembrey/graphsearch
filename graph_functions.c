0c8d5f73ea286450ddea9f8e4510fd71add09966
Nick Embrey <cne.embrey@gmail.com>
2016-11-13 15:01:41 -0800

    Implement graph title reading.

--- graph_functions.c
+++ graph_functions.c
@@ -3,7 +3,12 @@
 #include "sgraph.h"
 
 void read_input(char * filename, node start_node) {
+    FILE * read_file;
+    read_file = fopen(filename, "r");
+    char read_string[20];
 
+    fscanf(read_file, "%s", read_string);
+    printf("\n%s\n", read_string);
     return;
 }
 
